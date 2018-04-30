b57657bc1321df766fe9bc4789ee621b4536aeb8
vsakkas <vasileios.sakkas96@gmail.com>
2018-04-30 23:22:32 +0300

    rect now contains (min_x, min_y, max_x, max_y) instead of (left, top, width, height)

--- selectivesearch/selectivesearch.py
+++ selectivesearch/selectivesearch.py
@@ -1,10 +1,6 @@
 import numpy as np
 from skimage import color, feature, segmentation, util
 
-# "Selective Search for Object Recognition" by J.R.R. Uijlings et al.
-#
-#  - Modified version with LBP extractor for texture vectorization
-
 
 def _generate_segments(im_orig, scale, sigma, min_size):
     '''
@@ -239,7 +235,7 @@ def selective_search(im_orig, scale=1.0, sigma=0.8, min_size=50, color_bins=25, 
         regions : array of dict
             [
                 {
-                    'rect': (left, top, width, height),
+                    'rect': (min_x, min_y, max_x, max_y),
                     'labels': [...],
                     'size': component_size
                 },
@@ -296,53 +292,9 @@ def selective_search(im_orig, scale=1.0, sigma=0.8, min_size=50, color_bins=25, 
         regions.append({
             'rect': (
                 r['min_x'], r['min_y'],
-                r['max_x'] - r['min_x'], r['max_y'] - r['min_y']),
+                r['max_x'], r['max_y']),
             'size': r['size'],
             'labels': r['labels']
         })
 
     return img, regions
-
-
-import skimage.data
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-
-
-def main():
-
-    # loading astronaut image
-    img = skimage.data.astronaut()
-
-    # perform selective search
-    img_lbl, regions = selective_search(
-        img, scale=500, sigma=0.9, min_size=10)
-
-    candidates = set()
-    for r in regions:
-        # excluding same rectangle (with different segments)
-        if r['rect'] in candidates:
-            continue
-        # excluding regions smaller than 2000 pixels
-        if r['size'] < 2000:
-            continue
-        # distorted rects
-        x, y, w, h = r['rect']
-        if w / h > 1.2 or h / w > 1.2:
-            continue
-        candidates.add(r['rect'])
-
-    # draw rectangles on the original image
-    fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(6, 6))
-    ax.imshow(img)
-    for x, y, w, h in candidates:
-        print(x, y, w, h)
-        rect = mpatches.Rectangle(
-            (x, y), w, h, fill=False, edgecolor='red', linewidth=1)
-        ax.add_patch(rect)
-
-    plt.show()
-
-
-if __name__ == "__main__":
-    main()
